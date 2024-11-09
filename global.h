501cf8a046b0fd7aaa2f77a738214151841f02f1
hzy <hzy0052@foxmail.com>
2024-11-09 19:29:01 +0800

    feat:添加 ReqId, Modules, ErrorCodes 枚举类型

---
+++ global.h
@@ -0,0 +1,19 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+enum ReqId {
+    ID_GET_VARIFY_CODE = 1001, // 获取验证码
+    ID_REQ_USER = 1002, // 注册用户
+};
+
+enum Modules {
+    REGISTERMOD = 0, // 注册模块
+};
+
+enum ErrorCodes {
+    SUCCESS = 0,
+    ERR_JSON = 1,
+    ERR_NETWORK = 2,
+};
+
+#endif // GLOBAL_H
