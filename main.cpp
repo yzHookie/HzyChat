5edf3cd73752ecc534c5f348a4f63a287ea8bedf
hzy <hzy0052@foxmail.com>
2024-11-09 20:22:55 +0800

    feat:添加config.ini配置文件

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #include <QApplication>
 #include <QFile>
+#include <QDir>
+#include <QSettings>
+#include "global.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -14,6 +17,18 @@ int main(int argc, char *argv[])
     } else {
         qDebug("open failed");
     }
+
+    // 读取配置文件
+    QString file_name = "config.ini";
+    QString app_path = QCoreApplication::applicationDirPath();
+
+    // QDir::separator() 为分割符，可处理linux和windows不同的斜杠
+    QString config_path = QDir::toNativeSeparators(app_path + QDir::separator() + file_name);
+    QSettings settings(config_path, QSettings::IniFormat);
+    QString gate_host = settings.value("GateServer/host").toString();
+    QString gate_port = settings.value("GateServer/port").toString();
+    gate_url_prefix = "http://" + gate_host + ":" + gate_port;
+
     MainWindow w;
     w.show();
     return a.exec();
