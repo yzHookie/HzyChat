bafdccaf7582f9dfe463d6d8f4b17270e89d99cd
hzy <hzy0052@foxmail.com>
2024-11-09 19:30:57 +0800

    feat:新增发送验证码请求

--- register_dialog.cpp
+++ register_dialog.cpp
@@ -1,8 +1,10 @@
 #include "register_dialog.h"
 #include "ui_register_dialog.h"
 #include "util.h"
+#include "http_mgr.h"
 #include <QRegularExpression>
-
+#include <QJsonObject>
+#include <QJsonDocument>
 static QRegularExpression regex;
 
 RegisterDialog::RegisterDialog(QWidget *parent)
@@ -14,7 +16,10 @@ RegisterDialog::RegisterDialog(QWidget *parent)
 
     ui->err_tip->setProperty("state", "normal");
     util::repolish(ui->err_tip);
-    regex.setPattern(R"(\w+)(\.|_)?(\w*)@(\w+)(\.(\w+))+)");
+    regex.setPattern(R"((\w+)(\.|_)?(\w*)@(\w+)(\.(\w+))+)");
+    connect(HttpMgr::GetInstance().get(), &HttpMgr::SigReqModFinish, this, &RegisterDialog::SlotReqModFinish);
+
+    InitHttpHandlers();
 }
 
 RegisterDialog::~RegisterDialog()
@@ -28,10 +33,29 @@ void RegisterDialog::OnGetCodeClicked() {
     bool match = regex.match(email).hasMatch();
     if (match) {
         // 发送 http 验证码
+        QJsonObject json_obj;
+        json_obj["email"] = email;
+        HttpMgr::GetInstance()->PostHttpReq(QUrl("http://localhost:8080/get_varifycode"), json_obj, ReqId::ID_GET_VARIFY_CODE, Modules::REGISTERMOD);
     } else {
         ShowTip("邮箱地址格式不正确", false);
     }
 
+}
+
+void RegisterDialog::SlotReqModFinish(ReqId id, QString res, ErrorCodes err) {
+    if (err != ErrorCodes::SUCCESS) {
+        ShowTip(tr("网络请求错误"), false);
+        return;
+    }
+
+    // 序列化后才能发给后台
+    QJsonDocument json_doc = QJsonDocument::fromJson(res.toUtf8());
+    if (json_doc.isNull() || !json_doc.isObject()) {
+        ShowTip(tr("json解析失败"), false);
+        return;
+    }
+
+    handlers_[id](json_doc.object());
 }
 
 void RegisterDialog::ShowTip(QString tip, bool ok) {
@@ -42,4 +66,18 @@ void RegisterDialog::ShowTip(QString tip, bool ok) {
     }
     ui->err_tip->setText(tip);
     util::repolish(ui->err_tip);
+}
+
+void RegisterDialog::InitHttpHandlers() {
+    // 处理获取验证码回包的逻辑
+    handlers_.insert(ReqId::ID_GET_VARIFY_CODE, [this](const QJsonObject& json_obj) {
+        int error = json_obj["error"].toInt();
+        if (error != ErrorCodes::SUCCESS) {
+            ShowTip(tr("参数错误"), false);
+            return;
+        }
+        auto email = json_obj["email"].toString();
+        ShowTip(tr("验证码已经发送到邮箱，注意查收"), true);
+        qDebug() << "email is " << email;
+    });
 }
--- register_dialog.h
+++ register_dialog.h
@@ -2,7 +2,7 @@
 #define REGISTER_DIALOG_H
 
 #include <QDialog>
-
+#include "global.h"
 /******************************************************************************
  *
  * @file       register_dialog.h
@@ -24,9 +24,15 @@ public:
     ~RegisterDialog();
 public slots:
     void OnGetCodeClicked();
+    void SlotReqModFinish(ReqId id, QString res, ErrorCodes err);
 private:
     void ShowTip(QString tip, bool ok);
+    /**
+     * @brief 注册处理回包的逻辑
+     */
+    void InitHttpHandlers();
     Ui::RegisterDialog *ui;
+    QMap<ReqId, std::function<void(const QJsonObject&)>> handlers_;
 };
 
 #endif // REGISTER_DIALOG_H
